0ba1630db557ea1ae4fa5df898ca84562d4199dd
Shivam chaudhary <137470229+shivamgithubok@users.noreply.github.com>
2025-02-21 08:36:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
-from textSummarizer.pipeline.prediction import PredictionPipeline
+from textsummarizer.pipeline.prediction import PredictionPipeline
 
 app = FastAPI()
 
